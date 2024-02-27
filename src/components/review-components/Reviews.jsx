2bed23141c998d05786e28701c63204adbdd27fe
jaycossey <jaycossey@live.com>
2024-02-27 16:38:40 +0000

    remedied rendering structure, need to work out how to render the individual reviews and how to pass data to that component

--- src/components/review-components/Reviews.jsx
+++ src/components/review-components/Reviews.jsx
@@ -8,13 +8,23 @@ import fetchData from "../utils/fetchData";
 
 const Reviews = () => {
     const [cards, setCards] = useState();
+    const [singleReview, setSingleReview] = useState(<Loading />)
+    const [data, setData] = useState(0);
+    const [individualReviewToggle, setIndividualReviewToggle] = useState(false);
 
+    // needs to handle the toggle of rendered data and 
+    // pass data to individual review ONLY
     const handleClick = (index) => {
         console.log("click", index);
+        setIndividualReviewToggle(true);
+        // THIS IS THE HIGH LEVEL OF WHAT I NEED TO DO, BREAK IT DOWN!!!!!!!
+        // setSingleReview(<ReviewDisp bandData={data[index]} onClick={setIndividualReviewToggle(false)} />)
     }
 
+    // dont fuck with this
     const handleData = (data) => {
         let dataCopy = data;
+        setData(dataCopy);
         // console.log(dataCopy);
         setCards(dataCopy.map((artist, i) => {
             return (<BandCard 
@@ -28,6 +38,7 @@ const Reviews = () => {
         // console.log(cards);    
     }
 
+    // dont fuck with this
     useEffect(() => {
         fetchData('./reviews.json')
             .then(res => res.reviews)
@@ -37,22 +48,38 @@ const Reviews = () => {
             .catch(err => console.error(err));
     }, [])
 
-    return (
-        <div className="pt-16
-                        pb-8
-                        h-screen
-                        bg-fixed
-                        bg-gradient-to-r 
-                        from-orange-500 
-                        via-orange-300 
-                        to-blue-400">
+    if (!individualReviewToggle) {
+        return (
+            <div className="pt-16
+                            pb-8
+                            h-screen
+                            bg-fixed
+                            bg-gradient-to-r 
+                            from-orange-500 
+                            via-orange-300 
+                            to-blue-400">
 
-            {/* Review Select inst */}
-            <Container content={"Our Staff have listened to some incredible bands, be that live or their studio work, browse our reviews below."} />
-            <Container content={cards ? cards : <Loading />} />
+                {/* Review Select inst */}
+                <Container content={"Our Staff have listened to some incredible bands, be that live or their studio work, browse our reviews below."} />
+                <Container content={cards ? cards : <Loading />} />
 
-        </div>
-    );
+            </div>
+        );
+    } else {
+        return (
+            <div className="pt-16
+                            pb-8
+                            h-screen
+                            bg-fixed
+                            bg-gradient-to-r 
+                            from-orange-500 
+                            via-orange-300 
+                            to-blue-400">
+                <Container content={singleReview} />
+            </div>
+
+        );
+    }
 }
 
 export default Reviews;
