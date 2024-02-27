bb97b4f48e218d74722b1c631dfab6b7e431aa06
jaycossey <jaycossey@live.com>
2024-02-27 12:24:24 +0000

    added better structure to maintain state after re-renders

--- src/components/review-components/Reviews.jsx
+++ src/components/review-components/Reviews.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
 import Container from "../container-components/Container";
-import ReviewGrid from './ReviewGrid';
+import Loading from "./Loading";
+import ReviewDisp from "./ReviewDisp";
+import BandCard from "./BandCard";
 
 const Reviews = () => {
+    const [renderedState, setRenderedState] = useState(<Loading />);
+    const [fetchResults, setFetchResults] = useState();
+    const [allReviews, setAllReviews] = useState([]);
 
+    fetch('./reviews.json')
+    .then(res => res.json())
+    .then(data => {
+        setFetchResults(data.reviews);
+        setAllReviews(data.reviews.map((artist, i) =>
+        <BandCard
+            key={i}
+            name={artist.band} 
+            image={artist.image} 
+            album={artist.title}
+            onClick={() => handleClick(i)}
+            />
+    ))
+    setRenderedState(allReviews)})
+    .catch(err => console.error(err));
+
+
+
+    
     return (
         <div className="pt-16
                         pb-8
@@ -15,7 +40,7 @@ const Reviews = () => {
 
             {/* Review Select inst */}
             <Container content={"Our Staff have listened to some incredible bands, be that live or their studio work, browse our reviews below."} />
-            <Container content={<ReviewGrid />} />
+            <Container content={renderedState} />
 
         </div>
     );
