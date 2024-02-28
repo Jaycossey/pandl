b0dde2d00a273280324800c18797c0f54af86434
jaycossey <jaycossey@live.com>
2024-02-28 16:24:12 +0000

    edited staff details

--- src/components/staff-components/StaffGrid.jsx
+++ src/components/staff-components/StaffGrid.jsx
@@ -22,7 +22,7 @@ const StaffGrid = (props) => {
             {/* Currrently just has small hover animation
             want to add in feature which renders band review cards by that staff member. it should then 
             take you to that review on the band page when clicked*/}
-            <StaffCard name="Phil" job="Owner" profile="images/phil.png" genre="Tech-Metal & Pop Punk" />
+            <StaffCard name="Phil" job="Co-Owner & Reviewer" profile="images/phil.png" genre="Tech-Metal & Pop Punk" />
             <StaffCard name="Laura" job="Co-Owner & Editor" profile="images/laura.png" genre="Alternative & Pop Punk" />
             <StaffCard name="Kris" job="Reviewer" profile="images/kris.png" genre="Alternative, Indie, Rock & Metal" />
             <StaffCard name="Josi" job="Reviewer" profile="images/josi.png" genre="Metalcore & Pop Punk" />
